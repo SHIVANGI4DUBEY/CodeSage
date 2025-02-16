834c8ca3b8bc9ded52b5c66ffbee9b29124843a1
SHIVANGI DUBEY <144920253+SHIVANGI4DUBEY@users.noreply.github.com>
2025-02-16 17:16:19 +0530

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-require ('dotenv').config();
-const app=require('./src/app');
-
-//as server starts on port 3000 callback fn run when server is oin ready state
-app.listen(3000,()=>{
-console.log("Server is running on port 3000");
-})
